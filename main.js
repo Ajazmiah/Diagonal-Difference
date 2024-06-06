49389aeb20f0cb1d1ac51e89f0027c3ed2f599c0
Ajaz Miah <30759359+Ajazmiah@users.noreply.github.com>
2024-06-05 23:39:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 ///The left-to-right diagonal: 1 + 5 + 9=15 . The right to left diagonal:3 + 5 + 9= 17   
 //Their absolute difference is . | 15 - 17| = 2;
  feature-A
-
+ 
 // I MADE THIS CHANGE
 // I added this comment
 master
